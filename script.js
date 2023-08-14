740e0c1a5cd5326c47b8437b5c5738e9d27ee56d
Matt Pearson <mattpearson94@gmail.com>
2023-08-14 10:32:37 +0100

    Added the board and food

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-// board
-let boxSize = 25;
-let rows = 20;
-let columns = 20;
-let board;
-let context;
+let board = document.getElementById('board')
+let xAxis = 0
+// let yAxis = xAxis
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = rows * boxSize;
-    board.width = columns * boxSize;
-    context = board.getContext("2d");
-    update();
+board.style.width = '300px'
+board.style.height = '300px'
+// board.style.backgroundColor = 'white'
+
+function drawGrid() {
+    for (let i = 0; i < 12; i++) {
+        for (let y = 0; y < 12; y++) {
+            let div = document.createElement('div')
+            div.setAttribute('id',`${xAxis},${y}`)
+            div.style.backgroundColor = 'white'
+            div.style.width = '25px'
+            div.style.height = '25px'
+            board.appendChild(div)
+        }
+    xAxis += 1
+    }
 }
 
-function update() {
-    context.fillStyle = 'grey';
-    context.fillRect(0, 0, board.width, board.height);
+drawGrid()
+
+window.onload = (placeFood())
+
+function placeFood() {
+    let random = `${Math.floor(Math.random() * 12)},${Math.floor(Math.random() * 12)}`
+    let plots = document.getElementById(random)
+    plots.style.backgroundColor = 'red'
+
 }
+
